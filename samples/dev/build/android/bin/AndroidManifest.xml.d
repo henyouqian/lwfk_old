66ce059858345d92289fa987844f70b578868ff1
liwei <henyouqian@gmail.com>
2013-07-09 00:39:24 +0800

    lw

--- samples/dev/build/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/weili/Documents/projects/2013/lwfk/samples/dev/build/android/bin/AndroidManifest.xml : \
-/Users/weili/Documents/projects/2013/lwfk/samples/dev/build/android/AndroidManifest.xml \
--- samples/dev/build/android/gen/com/powervr/OGLES2IntroducingPFX/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.powervr.OGLES2IntroducingPFX;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
